9c5af913d7738b9981296112c73bcf5d91c03cf1
sparta <spartagus77@gmail.com>
2017-12-22 18:40:27 +0530

    csv json file read and table formation

--- src/app/csv-dynamic-table/csv-dynamic-table.component.ts
+++ src/app/csv-dynamic-table/csv-dynamic-table.component.ts
@@ -7,106 +7,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CsvDynamicTableComponent implements OnInit {
 
-tabledata: any;
-csvtext:any;
-jsonData:JSON;
-header:any;
+  csvtext:any;
+  jsonData:any;
+  header:Array<string>;
 
-constructor(){}
+  constructor(){}
 
-ngOnInit(){
-}
+  ngOnInit(){ }
 
-onFileSelect(input: HTMLInputElement) {
+  onFileSelect(input: HTMLInputElement) {   
+    const files = input.files;
+    if (files && files.length) {
+      let tempData = this;
+      let len = files[0].name.length;
+      let filetype = files[0].name.split(".");
 
-const files = input.files;
+      console.log("Filename: " + files[0].name); 
+      console.log("Type: " + filetype[1]);
+      console.log("Size: " + files[0].size + " bytes");
 
-if (files && files.length) {
-    let tempData = this;
-    console.log("Filename: " + files[0].name);
-    console.log("Type: " + files[0].type);
-    console.log("Size: " + files[0].size + " bytes");
+      const fileToRead = files[0];
 
-    const fileToRead = files[0];
+      const fileReader = new FileReader();
+      fileReader.onload = function(fileLoadedEvent){
+        if(filetype[1] == 'csv'){
+          tempData.csvtext = fileReader.result;
+          tempData.csvInput();
+        }
+        else if(filetype[1] == 'json'){
+          tempData.jsonData = JSON.parse(fileReader.result);
+          tempData.jsonInput();
+        }
+        else{
+          tempData.errorInput();
+        }          
+      };
+      fileReader.readAsText(files[0]);
+    }      
+  }
+     
+  csvInput(){    
+    this.jsonData = JSON.parse(this.csv2JSON(this.csvtext));
+    this.header = Object.keys(this.jsonData[0]);  
+    console.log("CSV : "+this.csvtext);
+    console.log("JSON : "+this.csv2JSON(this.csvtext));      
+    console.log("header : ===> "+this.header);
+  }
 
-    const fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent){
-      tempData.csvtext = fileReader.result;
-      tempData.test();
-    };
-    fileReader.readAsText(files[0]);
-  }      
-}
-   
-test(){
-  console.log('this.csvtext======');console.log(this.csvtext);
-  this.jsonData = JSON.parse(this.csvJSON(this.csvtext));
-  console.log("CSV : "+this.csvtext);
-  console.log("JSON : "+this.jsonData);
-}
+  jsonInput(){    
+    this.header = Object.keys(this.jsonData[0]);
+    console.log("JSON : ===> "+JSON.stringify(this.jsonData));
+    console.log("JSON.parse : ===> "+this.jsonData);    
+    console.log("header : ===> "+this.header);
+  }
 
-//var csv is the CSV file with headers
-csvJSON(csv){
-  var lines=csv.split("\r\n");
-  var result = [];
-                                                 // var tdata;
-  
+  errorInput(){    
+    this.header = null;
+    this.jsonData = null;
+    alert("Warning: input type is not csv / json type");
+  }
 
-  var headers=lines[0].split(",");
-  this.header = headers;
-  console.log("header : "+this.header);
-  
-  for(var i=1;i<lines.length-1;i++){
-    var obj = {};
-                                                   //var arr =[];
-    var currentline=lines[i].split(",");
 
-    for(var j=0;j<currentline.length;j++){
-      obj[headers[j]] = currentline[j];
-                                                   // tdata[i-1][j] = currentline[j];
+  //var csv is the CSV file with headers
+  csv2JSON(csv){
+    var lines=csv.split("\n");
+    var result = []; 
+
+    var headers=lines[0].split(",");
+    
+    for(var i=1;i<lines.length-1;i++){
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<currentline.length;j++){
+        obj[headers[j]] = currentline[j];
+      }
+      result.push(obj);
+                                                      
     }
-    result.push(obj);
-                                                    
+   // return result; //JavaScript object
+    return JSON.stringify(result); //JSON
   }
-                                                    // this.tabledata = tdata;
-                                                    //console.log("tabledata====== : "+this.tabledata);
- // return result; //JavaScript object
-  return JSON.stringify(result); //JSON
-}
-
-/*csvInput;
-jsonInput:any;
-
-  ngOnInit() {
-  	this.csvInput = "assets/csv/Sample.csv";
-  	this.jsonInput = JSON.parse(this.csvJSON(this.csvInput));
-  	console.log("CSV : "+this.csvInput);
-  	console.log("JSON : "+this.jsonInput);
-  }
-
- 
- //var csv is the CSV file with headers
- csvJSON(csv){
-  var lines=csv.split("\n");
-
-  var result = [];
-
-  var headers=lines[0].split(",");
-
-  for(var i=1;i<lines.length;i++){
-	  var obj = {};
-	  var currentline=lines[i].split(",");
-
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
-
-	  result.push(obj);
-  }
-  console.log("result : "+result);
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
-}*/
-
 }
 
